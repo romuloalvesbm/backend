a9901dfb0638a47b6eab7831b7c4bdcb2cefec89
romuloalvesbm <romuloalves.br@gmail.com>
2022-08-29 13:22:01 -0300

    Revert "Correcao"
    This reverts commit b93adf3dbe4e4becece92295d804432774b62652.

--- Projeto.Backend/Proejeto.Infra/AgendamentoColaboradorDto.cs
+++ Projeto.Backend/Proejeto.Infra/AgendamentoColaboradorDto.cs
@@ -8,7 +8,7 @@ namespace Projeto.Data.Dtos
     {
         public string chapa { get; set; }
         public string Nome { get; set; }
-        public int IdAgeCol2 { get; set; }
+        public int IdAgeCol { get; set; }
         public string Sobrenome { get; set; }   
         public string cidade { get; set; }
         public string Estado2 { get; set; }
