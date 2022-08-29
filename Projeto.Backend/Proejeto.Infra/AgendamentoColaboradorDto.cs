004168e8bcb80439cfd0248cd8a367e7c7765316
romuloalvesbm <romuloalves.br@gmail.com>
2022-08-29 07:23:30 -0300

    Arquivo pasta correta

---
+++ Projeto.Backend/Proejeto.Infra/AgendamentoColaboradorDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Data.Dtos
+{
+    public class AgendamentoColaboradorDto
+    {
+        public string chapa { get; set; }
+        public string Nome { get; set; }
+        public int IdAgeCol { get; set; }
+         public int Nome { get; set; }
+    }
+}
