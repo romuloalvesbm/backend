74050cf0da9ce6a3071e4acffbad94c5856dca6c
romuloalvesbm <58394591+romuloalvesbm@users.noreply.github.com>
2022-11-16 15:19:14 -0300

    Update AgendamentoTurmaDTO.cs
    Data obrigatoria

--- Projeto.Backend/Proejeto.Infra/AgendamentoTurmaDTO.cs
+++ Projeto.Backend/Proejeto.Infra/AgendamentoTurmaDTO.cs
@@ -9,7 +9,7 @@ namespace Projeto.Data.Dtos
         public int IdAgeTurma { get; set; }
         public string Evento { get; set; }
         public string Sala { get; set; }
-        public DateTime? Data { get; set; }
+        public DateTime Data { get; set; }
         public TimeSpan HoraInicio { get; set; }
         public TimeSpan HoraTermino { get; set; }
         public string Status { get; set; }       
