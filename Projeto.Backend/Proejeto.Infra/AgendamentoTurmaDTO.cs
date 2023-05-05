33fe920b8ff1392d428161474ff5618a5d4763ae
romuloalvesbm <58394591+romuloalvesbm@users.noreply.github.com>
2023-05-05 11:34:52 -0300

    Add Nome

--- Projeto.Backend/Proejeto.Infra/AgendamentoTurmaDTO.cs
+++ Projeto.Backend/Proejeto.Infra/AgendamentoTurmaDTO.cs
@@ -18,6 +18,8 @@ namespace Projeto.Data.Dtos
         public int? TotalColaborador { get; set; }
         public int Idade1 { get; set; }
         public int Idade2 { get; set; }
+        public string Idade2 { get; set; }
+        public string Nome { get; set; }
 
         public void CalcularIdade()
         {
